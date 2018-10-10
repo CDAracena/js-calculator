3e694d953d18f66e19372c3950738f648f7da0be
Christian Aracena <CDAracena@gmail.com>
2018-10-10 16:07:24 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,65 @@
+let inputDisplay = document.querySelector('.display-input');
+const userNumInput = document.querySelectorAll('.num');
+const clearButton = document.querySelector('.clear');
+const plus = document.querySelector('.plus');
+const subtract = document.querySelector('.minus')
+const equalBtn = document.querySelector('.equal-sign')
+
+
+let num1;
+let num2;
+let currentOperator;
+
+for (let i = 0; i < userNumInput.length;i++){
+  userNumInput[i].addEventListener('click', function(){
+    if (userNumInput[i].innerText != '='){
+    inputDisplay.innerText += userNumInput[i].innerText
+  }
+  })
+}
+
+const clearOut = ()=> {
+  if (inputDisplay){
+  inputDisplay.innerText = '';
+}
+}
+
+clearButton.addEventListener('click', function(){
+  clearOut();
+})
+
+
+function add(number1, number2){
+  let result = number1 + number2
+  inputDisplay.innerText = result;
+}
+
+function minus(number1, number2){
+let result = number1 - number2;
+inputDisplay.innerText = result;
+}
+
+plus.addEventListener('click', function(){
+  num1 = inputDisplay.innerText;
+  inputDisplay.innerText = '+';
+  currentOperator = 'plus';
+})
+
+subtract.addEventListener('click', function(){
+  num1 = inputDisplay.innerText;
+  inputDisplay.innerText = '-';
+  currentOperator = "minus";
+})
+
+
+equalBtn.addEventListener('click', function(){
+  num2 = inputDisplay.innerText;
+  num1 = parseInt(num1, 10);
+  num2 = parseInt(num2, 10);
+  if (currentOperator === "minus"){
+    minus(num1, num2);
+  } else if (currentOperator === "plus"){
+    add(num1, num2);
+  }
+  currentOperator = '';
+})
